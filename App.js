0f153262e24d6f2f35fadc99290794e2602e9e2f
sameer798 <sameershams.adi@gmailcom>
2024-05-05 02:29:26 +0530

    adding summary of meal

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header'
+import MealSummary from './components/Meals/MealSummary';
 
 
 function App() {
   return (
     <>
       <Header/>
+      <MealSummary/>
     </>
   );
 }
